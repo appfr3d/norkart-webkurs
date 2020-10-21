271a64637d24d995919ca9be73655030c125402f
Alfred Lieth Årøe <alfredla@stud.ntnu.no>
2020-10-21 17:03:26 +0200

    Updates

---
+++ precache-manifest.d1a36134be126e7be7296589a21c6ac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4a432862bff957fc81f31bbacfb488",
+    "url": "/norkart-webkurs/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart-webkurs/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "e3240b9dd3fcc8316946",
+    "url": "/norkart-webkurs/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart-webkurs/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/norkart-webkurs/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3240b9dd3fcc8316946",
+    "url": "/norkart-webkurs/static/js/main.fe0438f0.chunk.js"
+  },
+  {
+    "revision": "f817d818ef5b5420b13b",
+    "url": "/norkart-webkurs/static/js/runtime-main.f01a9071.js"
+  }
+]);
